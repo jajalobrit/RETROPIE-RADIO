b2be9615cd66e4921ec75568cddd0ba0794692e0
RETROWAVE-ARCADE <52551379+jajalobrit@users.noreply.github.com>
2019-09-25 16:23:56 -0300

    Update runcommand-onstart.sh

--- runcommand-onstart.sh
+++ runcommand-onstart.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
 sudo pkill radio.sh 
-sudo pkill IMAGEMRADIO.sh 
 sudo killall mplayer 
-sudo pkill TIME.sh
 sudo killall pngview
-omxplayer -b --layer 999999 RetroPie/roms/shutdown/L2/launching.mp4 > /dev/null 2>&1
