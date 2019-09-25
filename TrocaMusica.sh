3d31a3112fed8ffee4ad3f54e7eb96a17cc3ce0a
RETROWAVE-ARCADE <52551379+jajalobrit@users.noreply.github.com>
2019-09-25 19:26:32 -0300

    Update TrocaMusica.sh

--- TrocaMusica.sh
+++ TrocaMusica.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sudo echo "quit" > /usr/local/bin/RADIO/radio.fifo &
+sudo echo "quit" > home/pi/radio.fifo &
 sudo pkill -u pi pngview
 sleep 2
 sudo -u pi /usr/local/bin/RADIO/pngview -b 0 -l 999995 -y 15 -x 1580 /home/pi/RetroPie/roms/RADIO/$R.png;
