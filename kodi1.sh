c3c9985c03e119765f947cd1072fa35d090858da
RETROWAVE-ARCADE <52551379+jajalobrit@users.noreply.github.com>
2019-09-26 15:08:04 -0300

    Update kodi1.sh

--- kodi1.sh
+++ kodi1.sh
@@ -5,5 +5,6 @@ kodi >/dev/null
 if [ "$?" = "0" ]; then
 while pkill -0 kodi.bin 2>/dev/null; do sleep 1; done
 radio.sh
+find /usr/local/bin/RADIO/ -iname radio.sh -exec sudo sed -i 's,#sleep,sleep,g' {} \;
 fbset -depth 8 && fbset -depth 16 >/dev/null 2>&1
 fi
