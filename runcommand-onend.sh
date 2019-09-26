43a69d82ec575681d184c3c4824ca4e3a77c849b
RETROWAVE-ARCADE <52551379+jajalobrit@users.noreply.github.com>
2019-09-26 19:33:53 -0300

    Update runcommand-onend.sh

--- runcommand-onend.sh
+++ runcommand-onend.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-find /usr/local/bin/RADIO/ -iname radio.sh -exec sudo sed -i 's,#sleep,sleep,g' {} \; radio.sh &
+find /usr/local/bin/RADIO/ -iname radio.sh -exec sudo sed -i 's,#sleep,sleep,g' {} \; sudo /usr/local/bin/RADIO/radio.sh &
