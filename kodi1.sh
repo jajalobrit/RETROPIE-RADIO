7f714f48dc97f43bc9ccba12690ceeaf15dc500f
RETROWAVE-ARCADE <52551379+jajalobrit@users.noreply.github.com>
2019-09-26 15:06:53 -0300

    Update kodi1.sh

--- kodi1.sh
+++ kodi1.sh
@@ -1,5 +1,6 @@
 sudo pkill radio.sh
 sudo killall pngview
+find /usr/local/bin/RADIO/ -iname radio.sh -exec sudo sed -i 's,sleep,#sleep,g' {} \;
 kodi >/dev/null
 if [ "$?" = "0" ]; then
 while pkill -0 kodi.bin 2>/dev/null; do sleep 1; done
