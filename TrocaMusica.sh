a1482ed7cf11f87ddc6282045c64376cfa6d20c0
RETROWAVE-ARCADE <52551379+jajalobrit@users.noreply.github.com>
2019-09-28 15:34:25 -0300

    Update TrocaMusica.sh

--- TrocaMusica.sh
+++ TrocaMusica.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+#################################################### TROCA PARA A PRÓXIMA A RÁDIO #################################################
+#Autor: Jairo Lopes
+
 sudo echo "quit" > /usr/local/bin/RADIO/radio.fifo &
 sudo pkill -u pi pngview
 sleep 2
