7f090a89f087954bce42b716589531b30488853a
RETROWAVE-ARCADE <52551379+jajalobrit@users.noreply.github.com>
2019-09-29 16:14:06 -0300

    Delete TrocaMusica.sh

--- TrocaMusica.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-#################################################### TROCA PARA A PRÓXIMA A RÁDIO #################################################
-#Autor: Jairo Lopes
-
-sudo echo "quit" > /usr/local/bin/RADIO/radio.fifo &
-sudo pkill -u pi pngview
