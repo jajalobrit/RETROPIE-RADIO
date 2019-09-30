525232337d277ab1acebd93e463bf57a899a077d
RETROWAVE-ARCADE <52551379+jajalobrit@users.noreply.github.com>
2019-09-30 18:15:33 -0300

    Update kodi1.sh

--- kodi1.sh
+++ kodi1.sh
@@ -5,5 +5,6 @@ kodi >/dev/null
 if [ "$?" = "0" ]; then
 while pkill -0 kodi.bin 2>/dev/null; do sleep 1; done
 usr/local/bin/RADIO/radio2.sh &
+TIME.sh &
 fbset -depth 8 && fbset -depth 16 >/dev/null 2>&1
 fi
