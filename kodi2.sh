f2258757b0d202dac128b7d2ecda1220bedab1ee
RETROWAVE-ARCADE <52551379+jajalobrit@users.noreply.github.com>
2019-09-30 18:46:19 -0300

    Update kodi2.sh

--- kodi2.sh
+++ kodi2.sh
@@ -1,11 +1,15 @@
+#!/usr/bin/env bash
+
 sudo pkill TIME.sh
+sudo pkill -u root pngview
+sudo pkill -u pi pngview
 sudo pkill radio.sh
 sudo pkill radio2.sh
-sudo killall pngview
+sudo killall mplayer
 kodi >/dev/null
 if [ "$?" = "0" ]; then
 while pkill -0 kodi.bin 2>/dev/null; do sleep 1; done
-/usr/local/bin/RADIO/radio2.sh &
-TIME.sh &
+"/usr/local/bin/TIME.sh" &
+"/usr/local/bin/RADIO/radio2.sh" &
 fbset -depth 8 && fbset -depth 16 >/dev/null 2>&1
 fi
