9e52f4ba7ae43ae4f3dd6a25e14e104521ef536c
RETROWAVE-ARCADE <52551379+jajalobrit@users.noreply.github.com>
2019-10-01 03:05:58 -0300

    Update PausaRadio.sh

--- PausaRadio.sh
+++ PausaRadio.sh
@@ -3,4 +3,4 @@
 #################################################### PAUSA A RÁDIO #################################################
 #Autor: Jairo Lopes
 
-sudo echo "pt_step 1" > /usr/local/bin/RADIO/radio.fifo &
+sudo echo "pause" > /usr/local/bin/RADIO/radio.fifo &
