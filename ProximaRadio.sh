dfa0ff1fe71a01944eba365e5bc336298312a634
RETROWAVE-ARCADE <52551379+jajalobrit@users.noreply.github.com>
2020-06-16 16:25:09 -0300

    Delete ProximaRadio.sh

--- ProximaRadio.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-#################################################### TROCA PARA A PRÓXIMA A RÁDIO #################################################
-#Autor: Jairo Lopes
-
-sudo pkill mplayer
-sudo pkill -u pi pngview
