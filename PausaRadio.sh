26768668fdb532479eecaa065f205dc11818ee09
RETROWAVE-ARCADE <52551379+jajalobrit@users.noreply.github.com>
2020-06-16 16:25:00 -0300

    Delete PausaRadio.sh

--- PausaRadio.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-#################################################### PAUSA A RÁDIO #################################################
-#Autor: Jairo Lopes
-
-sudo echo "pause" > /usr/local/bin/RADIO/radio.fifo &
